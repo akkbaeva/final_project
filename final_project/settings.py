1c5ff58e5cc97823405359882a1038aaded1db59
Akkbaeva <akpaevafatima24@gmail.com>
2021-06-12 09:49:03 +0600

    wtf

--- final_project/settings.py
+++ final_project/settings.py
@@ -88,10 +88,19 @@ WSGI_APPLICATION = 'final_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default':
+        {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'pati_pr',
+            'USER': 'patisusers',
+            'PASSWORD': 'password',
+            'HOST': '127.0.0.1',
+            'PORT': '5432'
+        }
 }
 
 # Password validation
