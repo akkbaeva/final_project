b6c172c5966a0f97c155bfd15beebfbd37c6ffa7
Akkbaeva <akpaevafatima24@gmail.com>
2021-06-13 15:45:03 +0600

    wtf

--- final_project/settings.py
+++ final_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-6q@w6*&77ohwh+j6ze-6&70q4@3u+%lxmzhlq@@qd*mcfummp-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
